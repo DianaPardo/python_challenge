9958ba3fdce9af62c51edf1a7327b12e63ff7723
Diana Pardo Bonilla <diana.pardo12@gmail.com>
2019-03-09 22:39:48 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,20 +10,27 @@
 
 #Get path
 import os
+import os.path
 import csv
 
-# Establish the root path, data path and export output path
-root_path = os.path.join(os.getcwd(), ".")
-data_path = os.path.join(root_path, "raw_data")
-output_path = os.path.join(root_path, "output")
+# Set paths
+Bank_path = '/Users/dianapardo/Documents/GitHub/HomeWork-3/python_challenge/PyBank/Resources'
+csv_path = '/Users/dianapardo/Documents/GitHub/HomeWork-3/python_challenge/PyBank/Resources/budget_data'
+result_path = '/Users/dianapardo/Documents/GitHub/HomeWork-3/python_challenge/PyBank/Resources/Results'
 
-# Iterate through the listdir results
-filepaths = []
-for file in os.listdir(data_path):
-    if file.endswith(".csv"):
-        filepaths.append(os.path.join(data_path, file))
+#∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+# At first tried to join paths and concatenate accordingly to a loop. Could not do it, looked for support at StackOverflow and changed it to a full path. It seems like I was overiterating paths.
+# Set for loops
+#filepaths = []
+#for file in os.listdir(csv_path):
+    #if file.endswith(".csv"):
+        #filepaths.append(os.path.join(csv_path, file))
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Using csv.DictReader()
+
+# Reading CSV, previously had problems with this, did not get the proper codes for reading the files.
+#Set start of calculations
+filepaths='/Users/dianapardo/Documents/GitHub/HomeWork-3/python_challenge/PyBank/Resources/budget_data.csv'
 for file in filepaths:
     tot_revenue = 0
     month_count = 0
@@ -33,22 +40,27 @@ for file in filepaths:
     with open(file, newline="") as csvfile:
         csvreader = csv.DictReader(csvfile)
         for row in csvreader:
-            # Creating new revenue difference per month dictionary to calculate greatest inc/dec.
+            # Set first formula: increase/decrase
             rev_diff = {"rev_diff": int("{Revenue}".format(**row)) - revenue}
             rev_change = rev_change + int("{Revenue}".format(**row)) - revenue
             revenue = int("{Revenue}".format(**row))
             tot_revenue += revenue
             month_count += 1
             data_dict_list.append({**row, **rev_diff})
-        # Turn dictionary housing max and min rev_diff values as indvidual dicts outside of list.
+
+        # Set maximum and minimum values as a list
         increase_dict = dict(max(data_dict_list, key=lambda x:x["rev_diff"]))
         decrease_dict = dict(min(data_dict_list, key=lambda x:x["rev_diff"]))
-        # Pull date and rev_diff values for the corresponding greatest inc/dec months.
+
+
+        # Set increase/decrease month by month
         increase_date = increase_dict.get("Date")
         increase_revdiff = increase_dict.get("rev_diff")
         decrease_date = decrease_dict.get("Date")
         decrease_revdiff = decrease_dict.get("rev_diff")
-        # Adjust rev_diff to discount first row.
+
+
+        # Adjust start of calculations. First row as 0 (zero)
         first_row = data_dict_list[0]
         first_row_revdiff = first_row.get("rev_diff")
         rev_change = rev_change - first_row_revdiff
@@ -58,15 +70,15 @@ for file in filepaths:
         # The _, gets rid of the path. The , _ gets rid of the .csv.
         _, filename = os.path.split(file)
         filename, _ = filename.split(".csv")     
-        # Print the analysis to the terminal.
+        # Print ttable
         print(
             f"Financial Analysis - {filename}\n"
             f"----------------------------\n"
             f"Total Months: {month_count}\n" 
-            f"Total Revenue: ${tot_revenue}\n"
-            f"Average Revenue Change: ${avg_change}\n"
-            f"Greatest Increase in Revenue: {increase_date} (${increase_revdiff})\n"
-            f"Greatest Decrease in Revenue: {decrease_date} (${decrease_revdiff})\n"
+            f"Total: ${tot_revenue}\n"
+            f"Average Change: ${avg_change}\n"
+            f"Greatest Increase in Profits: {increase_date} (${increase_revdiff})\n"
+            f"Greatest Decrease in Profits: {decrease_date} (${decrease_revdiff})\n"
         )
 
         # Export a text file with the results.
@@ -76,8 +88,8 @@ for file in filepaths:
                 f"Financial Analysis: {filename}\n"
                 f"----------------------------\n"
                 f"Total Months: {month_count}\n" 
-                f"Total Revenue: ${tot_revenue}\n"
-                f"Average Revenue Change: ${avg_change}\n"
-                f"Greatest Increase in Revenue: {increase_date} (${increase_revdiff})\n"
-                f"Greatest Decrease in Revenue: {decrease_date} (${decrease_revdiff})\n"
+                f"Total: ${tot_revenue}\n"
+                f"Average Change: ${avg_change}\n"
+                f"Greatest Increase in Profits: {increase_date} (${increase_revdiff})\n"
+                f"Greatest Decrease in Profits: {decrease_date} (${decrease_revdiff})\n"
             )
