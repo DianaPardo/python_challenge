67a7f49d56eda9a0023d2a4c672712d754ea6094
Diana Pardo Bonilla <diana.pardo12@gmail.com>
2019-03-09 20:01:57 -0600

    This is PyBank homework

---
+++ PyBank/main.py
@@ -0,0 +1,14 @@
+#Could not open files. I struggled to open them inside the master folder, it appeared a message I could not solve the problem, marked a problem with the branches.
+#In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+#The total number of months included in the dataset
+#The net total amount of "Profit/Losses" over the entire period
+#The average of the changes in "Profit/Losses" over the entire period
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period
+
+import os
+import csv
+
+#Get path
+PyBank_path: os.path.join('budget_data.csv')
